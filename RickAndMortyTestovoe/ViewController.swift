1b3d08aa6f424c2e6ca721a95a16f5b88c0a2cd5
Vladislav Sidorin <sidorinvladislav3@gmail.com>
2023-12-05 21:26:10 +0300

    Initial Commit

---
+++ RickAndMortyTestovoe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyTestovoe
+//
+//  Created by Владислав Сидорин on 05.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
